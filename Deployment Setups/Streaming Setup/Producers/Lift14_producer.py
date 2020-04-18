e1ac53187bf2722d303ef8987ee1d0ad5040705a
kchakola <kenny_tony_chakola@sfu.ca>
2020-04-18 15:54:05 -0700

    Upload New File

---
+++ Deployment Setups/Streaming Setup/Producers/Lift14_producer.py
@@ -0,0 +1,35 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import pandas as pd
+import json
+import csv
+import datetime, time, random, threading
+
+rates = [24]
+
+def send_at(rate):
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x:dumps(x).encode('utf-8'))
+    interval = 1/rate
+    i = 0
+    while True:
+        with open('./Lift_samples/EDS_Lift14_sample.csv') as file:
+            reader = csv.DictReader(file, fieldnames = ["timestamp", "X", "Y", "Z"])
+            for row in reader:
+                row.update({'timestamp': str(datetime.datetime.now(tz=None))})
+                row.update({'id': 14})
+                row.update({'index': i})
+                data = json.loads(json.dumps(row))
+                producer.send('lift14',value=data)
+                i = i+1
+                sleep(interval)
+
+
+if __name__ == "__main__":
+    for rate in rates:
+        server_thread = threading.Thread(target=send_at, args=(rate,))
+        server_thread.setDaemon(True)
+        server_thread.start()
+
+    while 1:
+        time.sleep(1)
